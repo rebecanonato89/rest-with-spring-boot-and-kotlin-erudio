7a424a76a03ba36cfd7018581b7ef0a60682c9f0
Rebeca Nonato Domingos <rebecanonato89@gmail.com>
2024-07-10 18:57:57 -0300

    updated math

--- 06-parameterAndExceptionHandler/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/controller/MathController_old.kt
+++ 06-parameterAndExceptionHandler/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/controller/MathController_old.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
 
 @RestController
-class MathController {
+class MathController_old {
 
     val counter: AtomicLong = AtomicLong()
 
